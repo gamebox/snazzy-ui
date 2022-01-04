52440665566df8bd21b89539a9d7529d3f1486c2
Anthony Bullard <anthony.bullard@gmail.com>
2022-01-04 08:57:16 -0600

    Refactor: smaller size, types

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,18 +1,20 @@
 import esbuild from 'esbuild';
-import fs from 'fs';
+// import fs from 'fs';
 
 const outfile = 'dist/snazzy-ui.esm.js';
 
 await esbuild.build({
   entryPoints: ['src/index.ts'],
-  bundle: false,
+  bundle: true,
   minify: true,
+  format: 'esm',
+  sourcemap: true,
   outfile,
 });
 
-const mod = (await fs.promises.readFile(outfile)).toString();
-const newMod = mod.replace(
-  /"snabbdom"/,
-  '"https://cdn.skypack.dev/snabbdom@3.2.0"'
-);
-await fs.promises.writeFile(outfile, newMod);
+// const mod = (await fs.promises.readFile(outfile)).toString();
+// const newMod = mod.replace(
+//   /"snabbdom"/,
+//   '"https://cdn.skypack.dev/snabbdom@3.2.0"'
+// );
+// await fs.promises.writeFile(outfile, newMod);
--- src/index.ts
+++ src/index.ts
@@ -4,70 +4,63 @@ import {
   propsModule as pm,
   attributesModule as am,
   h,
-  jsx,
 } from 'snabbdom';
 
-type SnazzyConfig = {
-  view: Function;
-  init: Function | { [key: string]: any };
-  subscriptions: Function;
+import type {VNode, VNodeData} from 'snabbdom';
+
+type SnazzyConfig<S> = {
+  view: ViewFn<S>;
+  init: Update<S> | InitFn<S>;
+  subscriptions: Subscriptions<S>;
 };
+export type InitFn<S> = () => Update<S>;
+export type ViewFn<S> = (state: S, dispatch: DispatchFn<S>) => VNode;
+export type UpdateFn<S, P = any> = (state: S, payload: P) => Update<S>;
+export type Update<S> = Readonly<S | [S, Effect<S>[]]>;
+export type DispatchFn<S, P = any> = (type: UpdateFn<S, P>, payload: P) => void;
+export type EffectFn<S, P = any> = (dispatch: DispatchFn<S>, payload: P) => void;
+export type Effect<S, P = any> = Readonly<[EffectFn<S, P>, P]>;
+export type SubscriptionFn<S, P = any> = (dispatch: DispatchFn<S>, payload: P) => VoidFunction;
+export type Subscription<S, P = any> = [SubscriptionFn<S, P>, P];
+export type PossibleSubscription<S, P = any> = Readonly<false|Subscription<S, P>>;
+export type Subscriptions<S> = (state: S) => PossibleSubscription<S>[];
+type InternalState<S extends Object> = [
+  state: S,
+  effects: Effect<S>[],
+  subs: PossibleSubscription<S>[],
+  cleanups: (VoidFunction|null)[],
+  vnode: Element|VNode
+];
 
 // Snazzy - a lightweight vdom UI library
 const patch = snabbDomInit([pm, elm, am]);
-const app = (c: SnazzyConfig, mount: HTMLElement) => {
-  let queue: [Function, any][] = [],
-    s = typeof c.init === 'function' ? c.init() : c.init,
-    [state, effects] = Array.isArray(s) ? s : [s, []],
-    subs = c.subscriptions?.(state) ?? [],
-    cleanups = new Array(subs.length);
-  const dispatch = (type: Function, payload: any) =>
-    queue.push([type, payload]);
-  for (let x = 0; x < subs.length; x = x + 1) {
-    subs[x] &&
-      typeof subs[x][0] === 'function' &&
-      (cleanups[x] = subs[x][0](dispatch, subs[x][1]));
-  }
-  let vnode = c.view(state, dispatch);
-  const render = () => {
-    let newState = state,
-      newSubs = [];
-    for (const entry of queue) {
-      newState = entry[0](state, entry[1]);
-      if (Array.isArray(newState)) {
-        const [s, ...e] = newState;
-        (newState = s), (effects = [...effects, ...e]);
-      }
+const app = <S>(c: SnazzyConfig<S>, mount: HTMLElement): DispatchFn<S> => {
+  let queue: [Function, any][] = [[() => typeof c.init === 'function' ? c.init() : c.init, null]],
+    [state, effects, subs, cleanups, vnode]: InternalState<S> = [{} as S, [], [], [], mount],
+    scheduled: number|null, render: VoidFunction;
+  const dispatch = (t: Function, p: any) => queue.push([t, p]) && scheduled === null && (scheduled = requestAnimationFrame(render));
+  render = () => {
+    scheduled = null;
+    let newState: S = state, update, newSubs = subs, entry;
+    while ((entry = queue.shift(), entry)) {
+      update = entry[0](newState, entry[1]);
+      [newState, effects] = Array.isArray(update) ? [update[0], [...effects, ...update[1]]] : [update, effects];
     }
     if (newState !== state) {
-      (queue = []), (state = newState);
-      const newVnode = c.view(state, dispatch);
-      patch(vnode, newVnode);
-      vnode = newVnode;
-      c.subscriptions && (newSubs = c.subscriptions(state) ?? []);
-      for (let x = 0; x < newSubs.length; x = x + 1) {
-        if (!subs[x] && newSubs[x]) {
-          cleanups[x] = newSubs[x][0](dispatch, newSubs[x][1]);
-        } else if (subs[x] && !newSubs[x]) {
-          cleanups[x]?.() && (cleanups[x] = null);
-        }
+      vnode = patch(vnode, c.view(state = newState, dispatch));
+      newSubs = c.subscriptions?.(state) ?? [];
+      for (const [x, ns] of newSubs.entries()) {
+        (!subs[x] && ns) && (cleanups[x] = ns[0](dispatch, ns[1]));
+        (subs[x] && !ns) && cleanups[x]?.() && (cleanups[x] = null);
       }
-      for (const e of effects) {
-        e && e[0](dispatch, e[1]);
-      }
-      (subs = newSubs), (effects = []);
-    } else if (effects.length) {
-      queue = [];
-      for (const e of effects) {
-        e && e[0](dispatch, e[1]);
-      }
-      effects = [];
     }
-    requestAnimationFrame(render);
+    for (const e of effects) {
+      e[0](dispatch, e[1]);
+    }
+    effects = [], subs = newSubs;
   };
-  patch(mount, vnode);
-  requestAnimationFrame(render);
+  render();
   return dispatch;
 };
-
-export { h, jsx, app };
+// app: 1283 chars
+export { h, app, VNode, VNodeData };
